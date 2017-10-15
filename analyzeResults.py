06b278c47ed2fe32879896ea9dc59cbeb18b03af
Tushar Sharma <tsharma@HW15645.local>
2017-10-15 18:45:10 +0530

    Bug fix divied by 0,add failed information

--- analyzeResults.py
+++ analyzeResults.py
@@ -15,8 +15,9 @@ class analyze:
 			for db in self.results.keys():
 				for ql in self.results[db].keys():
 					for setting in self.results[db][ql].keys():
+						if 'NA' in self.results[db][ql][setting]:
+							self.exception_list[setting].append(ql)
 						if set(self.results[db][ql][setting])==set(['NA']):
-							exception_list[setting].append(ql)
 							continue
 						rank_dict[setting]+=float(sum([item for item in self.results[db][ql][setting] if item!='NA'])/len([item for item in self.results[db][ql][setting] if item!='NA']))
 						total_dict[setting]+=float(sum([item for item in self.results[db][ql][setting] if item!='NA']))
